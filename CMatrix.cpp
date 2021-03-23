05071ca905d04ada191e2c53b5c7c75003a2e7ee
Fobes <fobes_misterio@mail.ru>
2021-03-23 22:00:01 +0300

    Refactoring

--- CMatrix.cpp
+++ CMatrix.cpp
@@ -2,13 +2,6 @@
 #include "CMatrix.h"
 #include <math.h>
 
-#define MTXSET(M, _m11,_m12,_m13,_m14, _m21,_m22,_m23,_m24, _m31,_m32,_m33,_m34, _m41,_m42,_m43,_m44 ) do{ \
-		(M).m11=(_m11); (M).m12=(_m12); (M).m13=(_m13); (M).m14=(_m14);	\
-		(M).m21=(_m21); (M).m22=(_m22); (M).m23=(_m23); (M).m24=(_m24);	\
-		(M).m31=(_m31); (M).m32=(_m32); (M).m33=(_m33); (M).m34=(_m34);	\
-		(M).m41=(_m41); (M).m42=(_m42); (M).m43=(_m43); (M).m44=(_m44);	\
-	} while(0)
-
 CMatrix::CMatrix()
 {
 }
@@ -45,11 +38,10 @@ void CMatrix::Set(const CRotation& rotation, const CVector3& vtTranslation, floa
 	float lys = ly * fSin;
 	float lzs = lz * fSin;
 
-	MTXSET(*this,	
-		fScale * (lxlxomc + fCos),		fScale * (lxlyomc + lzs),	fScale * (lxlzomc - lys),	vtTranslation.m_fX,
-		fScale* (lxlyomc - lzs),		fScale * (lylyomc + fCos),	fScale * (lylzomc + lxs),	vtTranslation.m_fY,
-		fScale* (lxlzomc + lys),		fScale * (lylzomc - lxs),	fScale * (lzlzomc + fCos),	vtTranslation.m_fZ,
-		0.0f, 0.0f, 0.0f, 1.0f);
+	m11 = fScale * (lxlxomc + fCos); m12 = fScale * (lxlyomc + lzs);  m13 = fScale * (lxlzomc - lys);	 m14 = vtTranslation.m_fX;
+	m21 = fScale * (lxlyomc - lzs);	 m22 = fScale * (lylyomc + fCos); m23 = fScale * (lylzomc + lxs);	 m24 = vtTranslation.m_fY;
+	m31 = fScale * (lxlzomc + lys);	 m32 = fScale * (lylzomc - lxs);  m33 = fScale * (lzlzomc + fCos);	 m34 = vtTranslation.m_fZ,
+	m41 = 0.0f;						 m42 = 0.0f;					  m43 = 0.0f;						 m44 = 1.0f;
 }
 
 void CMatrix::Set(const CRotation& rotation)
@@ -74,11 +66,10 @@ void CMatrix::Set(const CRotation& rotation)
 	float lys = ly * fSin;
 	float lzs = lz * fSin;
 
-	MTXSET(*this,
-		(lxlxomc + fCos),	(lxlyomc + lzs),	(lxlzomc - lys),	0,
-		(lxlyomc - lzs),	(lylyomc + fCos),	(lylzomc + lxs),	0,
-		(lxlzomc + lys),	(lylzomc - lxs),	(lzlzomc + fCos),	0,
-		0.0f, 0.0f, 0.0f, 1.0f);
+	m11 = (lxlxomc + fCos); m12 = (lxlyomc + lzs);	m13 = (lxlzomc - lys);  m14 = 0;
+	m21 = (lxlyomc - lzs);	m22 = (lylyomc + fCos); m23 = (lylzomc + lxs);  m24 = 0;
+	m31 = (lxlzomc + lys);	m32 = (lylzomc - lxs);	m33 = (lzlzomc + fCos); m34 = 0;
+	m41 = 0.0f;				m42 = 0.0f;				m43 = 0.0f;				m44 = 1.0f;
 }
 
 void CMatrix::SetIdentity()
@@ -94,38 +85,35 @@ const CMatrix& CMatrix::Perspective(CMatrix* pDest, float fYFOV, float fAspect, 
 	float fScaleY = 1.0f / fTanHalfFov;
 	float fScaleX = fScaleY / fAspect;
 
-	MTXSET(*pDest,
-		fScaleX,	0.0f,		0.0f,					0.0f,
-		0.0f,		fScaleY,	0.0f,					0.0f,
-		0.0f,		0.0f,		fFar / (fFar - fNear),		-fNear * fFar / (fFar - fNear),
-		0.0f,		0.0f,		1.0f,					0.0f);
+	pDest->m11 = fScaleX;  pDest->m12 = 0.0f;       pDest->m13 = 0.0f;					pDest->m14 = 0.0f;
+	pDest->m21 = 0.0f;     pDest->m22 = fScaleY;    pDest->m23 = 0.0f;					pDest->m24 = 0.0f;
+	pDest->m31 = 0.0f;	   pDest->m32 = 0.0f;	    pDest->m33 = fFar / (fFar - fNear);	pDest->m34 = -fNear * fFar / (fFar - fNear);
+	pDest->m41 = 0.0f;	   pDest->m42 = 0.0f;	    pDest->m43 = 1.0f;					pDest->m44 = 0.0f;
 
 	return *pDest;
 }
 
 const CMatrix& CMatrix::Mul(CMatrix* pAtoC, const CMatrix& AtoB, const CMatrix& BtoC)
 {
-	MTXSET(*pAtoC,
-		AtoB.m11 * BtoC.m11 + AtoB.m21 * BtoC.m12 + AtoB.m31 * BtoC.m13 + AtoB.m41 * BtoC.m14,
-		AtoB.m12 * BtoC.m11 + AtoB.m22 * BtoC.m12 + AtoB.m32 * BtoC.m13 + AtoB.m42 * BtoC.m14,
-		AtoB.m13 * BtoC.m11 + AtoB.m23 * BtoC.m12 + AtoB.m33 * BtoC.m13 + AtoB.m43 * BtoC.m14,
-		AtoB.m14 * BtoC.m11 + AtoB.m24 * BtoC.m12 + AtoB.m34 * BtoC.m13 + AtoB.m44 * BtoC.m14,
+	pAtoC->m11 = AtoB.m11 * BtoC.m11 + AtoB.m21 * BtoC.m12 + AtoB.m31 * BtoC.m13 + AtoB.m41 * BtoC.m14;
+	pAtoC->m12 = AtoB.m12 * BtoC.m11 + AtoB.m22 * BtoC.m12 + AtoB.m32 * BtoC.m13 + AtoB.m42 * BtoC.m14;
+	pAtoC->m13 = AtoB.m13 * BtoC.m11 + AtoB.m23 * BtoC.m12 + AtoB.m33 * BtoC.m13 + AtoB.m43 * BtoC.m14;
+	pAtoC->m14 = AtoB.m14 * BtoC.m11 + AtoB.m24 * BtoC.m12 + AtoB.m34 * BtoC.m13 + AtoB.m44 * BtoC.m14;
 
-		AtoB.m11 * BtoC.m21 + AtoB.m21 * BtoC.m22 + AtoB.m31 * BtoC.m23 + AtoB.m41 * BtoC.m24,
-		AtoB.m12 * BtoC.m21 + AtoB.m22 * BtoC.m22 + AtoB.m32 * BtoC.m23 + AtoB.m42 * BtoC.m24,
-		AtoB.m13 * BtoC.m21 + AtoB.m23 * BtoC.m22 + AtoB.m33 * BtoC.m23 + AtoB.m43 * BtoC.m24,
-		AtoB.m14 * BtoC.m21 + AtoB.m24 * BtoC.m22 + AtoB.m34 * BtoC.m23 + AtoB.m44 * BtoC.m24,
+	pAtoC->m21 = AtoB.m11 * BtoC.m21 + AtoB.m21 * BtoC.m22 + AtoB.m31 * BtoC.m23 + AtoB.m41 * BtoC.m24;
+	pAtoC->m22 = AtoB.m12 * BtoC.m21 + AtoB.m22 * BtoC.m22 + AtoB.m32 * BtoC.m23 + AtoB.m42 * BtoC.m24;
+	pAtoC->m23 = AtoB.m13 * BtoC.m21 + AtoB.m23 * BtoC.m22 + AtoB.m33 * BtoC.m23 + AtoB.m43 * BtoC.m24;
+	pAtoC->m24 = AtoB.m14 * BtoC.m21 + AtoB.m24 * BtoC.m22 + AtoB.m34 * BtoC.m23 + AtoB.m44 * BtoC.m24;
 
-		AtoB.m11 * BtoC.m31 + AtoB.m21 * BtoC.m32 + AtoB.m31 * BtoC.m33 + AtoB.m41 * BtoC.m34,
-		AtoB.m12 * BtoC.m31 + AtoB.m22 * BtoC.m32 + AtoB.m32 * BtoC.m33 + AtoB.m42 * BtoC.m34,
-		AtoB.m13 * BtoC.m31 + AtoB.m23 * BtoC.m32 + AtoB.m33 * BtoC.m33 + AtoB.m43 * BtoC.m34,
-		AtoB.m14 * BtoC.m31 + AtoB.m24 * BtoC.m32 + AtoB.m34 * BtoC.m33 + AtoB.m44 * BtoC.m34,
+	pAtoC->m31 = AtoB.m11 * BtoC.m31 + AtoB.m21 * BtoC.m32 + AtoB.m31 * BtoC.m33 + AtoB.m41 * BtoC.m34;
+	pAtoC->m32 = AtoB.m12 * BtoC.m31 + AtoB.m22 * BtoC.m32 + AtoB.m32 * BtoC.m33 + AtoB.m42 * BtoC.m34;
+	pAtoC->m33 = AtoB.m13 * BtoC.m31 + AtoB.m23 * BtoC.m32 + AtoB.m33 * BtoC.m33 + AtoB.m43 * BtoC.m34;
+	pAtoC->m34 = AtoB.m14 * BtoC.m31 + AtoB.m24 * BtoC.m32 + AtoB.m34 * BtoC.m33 + AtoB.m44 * BtoC.m34;
 
-		AtoB.m11 * BtoC.m41 + AtoB.m21 * BtoC.m42 + AtoB.m31 * BtoC.m43 + AtoB.m41 * BtoC.m44,
-		AtoB.m12 * BtoC.m41 + AtoB.m22 * BtoC.m42 + AtoB.m32 * BtoC.m43 + AtoB.m42 * BtoC.m44,
-		AtoB.m13 * BtoC.m41 + AtoB.m23 * BtoC.m42 + AtoB.m33 * BtoC.m43 + AtoB.m43 * BtoC.m44,
-		AtoB.m14 * BtoC.m41 + AtoB.m24 * BtoC.m42 + AtoB.m34 * BtoC.m43 + AtoB.m44 * BtoC.m44
-	);
+	pAtoC->m41 = AtoB.m11 * BtoC.m41 + AtoB.m21 * BtoC.m42 + AtoB.m31 * BtoC.m43 + AtoB.m41 * BtoC.m44;
+	pAtoC->m42 = AtoB.m12 * BtoC.m41 + AtoB.m22 * BtoC.m42 + AtoB.m32 * BtoC.m43 + AtoB.m42 * BtoC.m44;
+	pAtoC->m43 = AtoB.m13 * BtoC.m41 + AtoB.m23 * BtoC.m42 + AtoB.m33 * BtoC.m43 + AtoB.m43 * BtoC.m44;
+	pAtoC->m44 = AtoB.m14 * BtoC.m41 + AtoB.m24 * BtoC.m42 + AtoB.m34 * BtoC.m43 + AtoB.m44 * BtoC.m44;
 
 	return *pAtoC;
 }
